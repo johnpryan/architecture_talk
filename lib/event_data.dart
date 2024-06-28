ada12162b10e22437b70b9c93036d3227393a556
John Ryan <ryjohn@google.com>
2024-06-28 08:47:09 +0200

    Create the EventData class

---
+++ lib/event_data.dart
@@ -0,0 +1,26 @@
+import 'event.dart';
+
+class EventData {
+  Iterable<Event> eventsForDate(DateTime date) =>
+      _events.where((event) => _isSameDay(date, event.start));
+
+  Iterable<Event> get _events => _createMockData();
+
+  bool _isSameDay(DateTime a, DateTime b) =>
+      a.year == b.year && a.month == b.month && a.day == b.day;
+}
+
+Iterable<Event> _createMockData() sync* {
+  var names = ['Morning routine', 'Breakfast', 'Commute'];
+  var startTime = DateTime.now().copyWith(hour: 8, minute: 0, microsecond: 0);
+  final oneHour = Duration(hours: 1);
+
+  for (var name in names) {
+    yield Event(
+      name,
+      start: startTime,
+      end: startTime.add(oneHour),
+    );
+    startTime = startTime.add(oneHour);
+  }
+}
